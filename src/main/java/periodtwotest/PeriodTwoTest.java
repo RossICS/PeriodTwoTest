58dfb2a85576227f11822851f0c4aa93dc5e83bf
RossICS2022 <119336782+RossICS2022@users.noreply.github.com>
2023-12-12 10:53:39 -0500

    First commit.

---
+++ src/main/java/periodtwotest/PeriodTwoTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package periodtwotest;
+
+/**
+ *
+ * @author Administrator
+ */
+public class PeriodTwoTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Period Two Test");
+    }
+    
+}
